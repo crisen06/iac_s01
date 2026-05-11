592358a15f0a00d970ddcec9aa3fd5657efea30c
crisen06 <crisen.cordova@gmail.com>
2026-05-10 19:43:58 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,7 @@
-variable "resource_group_name" {
-  description = "Nombre del Resource Group asignado al alumno"
-  type        = string
-}
+
 
 variable "student_name" {
   description = "Nombre o iniciales del alumno (solo minúsculas y números)"
   type        = string
 }
 
-variable "student_id" {
-  description = "ID único del alumno (ej: 01, 02...)"
-  type        = string
-}
-
-variable "container_name" {
-  description = "Nombre del contenedor Blob"
-  type        = string
-  default     = "archivos-utec"
-}

05da10e9f8cd0af3ef49a6bf6ec054eb313f3bc0
crisen06 <crisen.cordova@gmail.com>
2026-05-10 19:54:32 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,7 @@ output "policy_name" {
   value = aws_iam_role_policy.test_policy.name
 }
 
-output "blob_endpoint" {
+output "role_name" {
   value = azurerm_storage_account.sa_utec.primary_blob_endpoint
 }
 
-output "primary_access_key" {
-  value     = azurerm_storage_account.sa_utec.primary_access_key
-  sensitive = true
-}
